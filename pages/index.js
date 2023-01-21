db8829ffca55130c6059d196788bd711a4b1b110
gillsukhman209 <gillsukhman209@gmail.com>
2023-01-20 21:35:48 -0800

    new update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import ProfilePic from "../public/kp.jpeg";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-gray-800 text-white">
-      <h1>This is kuldeep's website</h1>
-      <img src=""></img>
+    <div className="w-full h-[4000px] bg-gray-800 text-white flex flex-col  gap-20 items-center justify-start">
+      <Image
+        src="/kp.jpeg"
+        alt="me"
+        width="600"
+        height="600"
+        className="mt-[200px]"
+      />
+      <h1 className="text-5xl">Kuldeep Singh Pasla</h1>
+      <div className="flex gap-20 items-start justify-center ">
+        <div className="flex flex-col gap-10 text-white text-4xl items-center justify-center">
+          <h1>Age: 28</h1>
+          <h1>Lathrop CA</h1>
+          <h1>Volleyball / Tennis / Badminton</h1>
+        </div>
+      </div>
     </div>
   );
 }
